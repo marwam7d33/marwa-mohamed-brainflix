abc04a145a6b0c50dd86621cc29d3b31db948d4d
Marwa Mohamed <marwa.m7d@gmail.com>
2024-10-23 02:00:21 -0400

    include video player

--- marwa-mohamed-brainflix/src/App.jsx
+++ marwa-mohamed-brainflix/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.scss";
 import Header from "./Components/i/Header";
+import VideoPlayer from "./Components/i/VideoPlayer";
+
 function App() {
   return (
     <>
       <Header />
+      <VideoPlayer />
     </>
   );
 }
