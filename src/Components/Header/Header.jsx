038e13cbe4d8fdb3c6665a4432ac119e35f47daf
Jonathan Mazin <jmazin@brainstation.io>
2024-10-25 17:14:11 -0400

    pairing w Jon (header)

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,28 +3,26 @@ function Header() {
   return (
     <nav>
       <header className="header">
-        <img
-          src=".\src\assets\Logo\BrainFlix-logo.svg"
-          alt="BrainFlix"
-          className="header__logo"
-        />
+        <div className="header__img-container">
+          <img src=".\src\assets\Logo\BrainFlix-logo.svg" alt="BrainFlix" className="header__logo" />
+        </div>
         <div className="header__container">
           <input type="text" placeholder="Search" className="header__search" />
-          <div className="header__tablet--container"></div>
-          <img
-            src=".\src\assets\Images\Mohan-muruge.jpg"
-            alt="Profile"
-            className="header__avatar"
-          />
+          <img src=".\src\assets\Images\Mohan-muruge.jpg" alt="Profile" className="header__avatar" />
         </div>
         <div className="header__upload--outer">
           <button className="header__upload">
             <p className="header__text">
               <img src="\src\assets\Icons\upload.svg"></img>
-              <div className="header__upload--text">UPLOAD</div>
+              <span className="header__upload--text">UPLOAD</span>
             </p>
           </button>
         </div>
+        <img
+          src=".\src\assets\Images\Mohan-muruge.jpg"
+          alt="Profile"
+          className="header__avatar header__avatar--tablet"
+        />
       </header>
     </nav>
   );
